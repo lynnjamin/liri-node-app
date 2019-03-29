9b613682dd05cc948b5de279a84a46f21db0f4fe
Lynn Tang <tanglynn2@gmail.com>
2019-03-28 19:55:59 -0500

    spotify going..

--- liri.js
+++ liri.js
@@ -5,17 +5,21 @@ var moment = require('moment');
 moment().format();
 var axios = require("axios");
 var spotify = new Spotify(keys.spotify);
-var input = process.argv.slice(3).join(" ")
+var input = process.argv.slice(3).join("+")
+var command = process.argv[2];
 
-
+//////////////////
+// CONCERT-THIS //
+/////////////////
 function concertThis() {
      axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then
      (function(response){
-          for(var i = 0; i < response.data.length; i++){
+          for(var i = 0; i < 11; i++){
+          console.log("Event " + i);
           console.log("Venue Name : " + response.data[i].venue.name);
           console.log("Venue Location : " + response.data[i].venue.city + ", " + response.data[i].venue.country);
           console.log("Date of Event : " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
-          console.log("==========================================");
+          console.log("\n==========================================\n");
           }
      },
      function(error) {
@@ -24,27 +28,53 @@ function concertThis() {
      } else if (error.request) {
           console.log(error.request);
      } else {
-          console.log("Error", error.message);
+          console.log("Sorry, there are no concerts : ", error.message);
      }
      console.log(error.config);
    }
  )
 }
 
-concertThis();
+///////////////////////
+// SPOTIFY THIS SONG //
+///////////////////////
+function spotifyThis(){
+     spotify.search({ type: 'track', query: input }, function(err, data) {
+          if (err) {
+            return console.log("No songs are found: " + err);
+          }
+          console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
+          console.log("Song Title: " + data.tracks.items[0].name);
+          console.log("Preview Link " + data.tracks.items[0].preview_url);
+          console.log("Album: " + data.tracks.items[0].album.name);
+
+        });
+}
+      
 
 
 
-switch(input) {
+////////////////////////////////
+// Main function for commands //
+////////////////////////////////
+function liriBot() {
+     switch(command){
      case "concert-this" :
+     concertThis();
      break;
      case "spotify-this-song" :
+     spotifyThis();
      break;
      case "movie-this" :
+     movieThis();
      break;
      case "do-what-it-says" :
+     doWhatItSays();
      break;
-
-     default: 
+     default :
+     console.log("I don't understand");
+     }
 }
 
+//run program
+liriBot();
